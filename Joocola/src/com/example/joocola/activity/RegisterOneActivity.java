58442fd56258a98edea77203cbb902dca8445732
tcmaster <18600416253@163.com>
2014-07-07 22:56:57 +0800

    加了一些界面的初始逻辑

--- Joocola/src/com/example/joocola/activity/RegisterOneActivity.java
+++ Joocola/src/com/example/joocola/activity/RegisterOneActivity.java
@@ -1,24 +1,28 @@
 package com.example.joocola.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.joocola.R;
 
-public class RegisterOneActivity extends BaseActivity {
+public class RegisterOneActivity extends BaseActivity implements
+		OnClickListener {
 	// 获取验证码，下一步按钮
-	Button b_getAutoCode, b_nextStep;
+	private Button b_getAutoCode, b_nextStep;
 	// 用户名，验证码，密码，介绍人
-	EditText et_userName, et_code, et_password, et_introducer;
+	private EditText et_userName, et_code, et_password, et_introducer;
 	// 返回登陆界面
-	TextView tv_backLogin;
+	private TextView tv_backLogin;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		initView();
+		registerListener();
 	}
 
 	private void initView() {
@@ -29,5 +33,39 @@ public class RegisterOneActivity extends BaseActivity {
 		et_password = (EditText) findViewById(R.id.password_text);
 		et_introducer = (EditText) findViewById(R.id.introducerText);
 		tv_backLogin = (TextView) findViewById(R.id.backLogin);
+	}
+
+	private void registerListener() {
+		b_getAutoCode.setOnClickListener(this);
+		b_nextStep.setOnClickListener(this);
+		tv_backLogin.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.getAutoCode:
+			getAutoCode();
+			break;
+		case R.id.nextStep:
+			nextStep();
+			break;
+		case R.id.backLogin:
+			backLogin();
+		default:
+			break;
+		}
+	}
+
+	private void getAutoCode() {
+
+	}
+
+	private void nextStep() {
+
+	}
+
+	private void backLogin() {
+
 	}
 }
--- Joocola/src/com/example/joocola/activity/RegisterTwoActivity.java
+++ Joocola/src/com/example/joocola/activity/RegisterTwoActivity.java
@@ -1,11 +1,83 @@
 package com.example.joocola.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
 
-public class RegisterTwoActivity extends BaseActivity {
+import com.example.joocola.R;
+
+public class RegisterTwoActivity extends BaseActivity implements
+		OnClickListener {
+	// 用户头像
+	private ImageView iv_userPhoto;
+	// 从相册获取头像，拍照获取头像，完成注册
+	private Button b_fromGallery, b_takePicture, b_finishRegister;
+	// 昵称，生日
+	private EditText et_nickName, et_birthday;
+	// 性别选择
+	private RadioGroup rg_group;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		initView();
+		registerListener();
+	}
+
+	private void initView() {
+		iv_userPhoto = (ImageView) findViewById(R.id.photoImg);
+		b_fromGallery = (Button) findViewById(R.id.selectFromGallery);
+		b_takePicture = (Button) findViewById(R.id.takePicture);
+		b_finishRegister = (Button) findViewById(R.id.registerButton);
+		et_nickName = (EditText) findViewById(R.id.NickName_text);
+		et_birthday = (EditText) findViewById(R.id.birthdayText);
+		rg_group = (RadioGroup) findViewById(R.id.Group);
+	}
+
+	private void registerListener() {
+		b_finishRegister.setOnClickListener(this);
+		b_takePicture.setOnClickListener(this);
+		iv_userPhoto.setOnClickListener(this);
+		b_fromGallery.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.selectFromGallery:
+			getPhotoFromGallery();
+			break;
+		case R.id.takePicture:
+			getPhotoByTakePicture();
+			break;
+		case R.id.registerButton:
+			register();
+			break;
+		case R.id.photoImg:
+			clickPhoto();
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void getPhotoFromGallery() {
+
+	}
+
+	private void getPhotoByTakePicture() {
+
+	}
+
+	private void register() {
+
+	}
+
+	private void clickPhoto() {
+
 	}
 }
