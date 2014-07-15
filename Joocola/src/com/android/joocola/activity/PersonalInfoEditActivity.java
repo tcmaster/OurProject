3e718c0eeff276ae769e33e25a8dbdb90ed6b808
tcmaster <18600416253@163.com>
2014-07-15 23:46:41 +0800

    增加了几个界面，然后思考了一下人生

---
+++ Joocola/src/com/android/joocola/activity/PersonalInfoEditActivity.java
@@ -0,0 +1,21 @@
+package com.android.joocola.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.android.joocola.R;
+
+/**
+ * 个人资料编辑界面
+ * 
+ * @author lixiaosong
+ * 
+ */
+public class PersonalInfoEditActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_personal_info_edit);
+	}
+}
