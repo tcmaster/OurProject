26c7326857073c8171fda19b7e8a69f374053d73
bb <bb@172.17.1.143>
2014-07-17 11:45:33 +0800

    更新了constans

--- Joocola/src/com/android/joocola/utils/Constans.java
+++ Joocola/src/com/android/joocola/utils/Constans.java
@@ -25,5 +25,6 @@ public class Constans {
 	public static final String basedata_Drink = "Drink";
 	public static final String basedata_Revenue = "Revenue";
 	public static final String basedata_Height = "Height";
+	public static final String basedata_Hobby = "Hobby";
 	public static final String basedata_AppointCost = "AppointCost";
 }
