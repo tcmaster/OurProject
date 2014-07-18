f7906909dc59e008328288622affb6b1902e742b
tcmaster <18600416253@163.com>
2014-07-18 17:36:27 +0800

    增加了个人中心的一些逻辑，编写了适配器，布局等

--- Joocola/src/com/android/joocola/activity/PersonalInfoEditActivity.java
+++ Joocola/src/com/android/joocola/activity/PersonalInfoEditActivity.java
@@ -1,12 +1,26 @@
 package com.android.joocola.activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.joocola.R;
+import com.android.joocola.adapter.Dlg_ListView_Adapter;
+import com.android.joocola.app.JoocolaApplication;
+import com.android.joocola.entity.BaseDataInfo;
+import com.android.joocola.utils.CustomerDialog;
+import com.android.joocola.utils.CustomerDialog.CustomerViewInterface;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.lidroid.xutils.view.annotation.event.OnClick;
@@ -79,10 +93,21 @@ public class PersonalInfoEditActivity extends Activity {
 	@ViewInject(R.id.drink)
 	private TextView drink_tv;
 
+	/**
+	 * 基础数据
+	 */
+	private List<BaseDataInfo> baseDataInfos;
+	/**
+	 * 下面两个是用于临时存放选择数据的变量
+	 */
+	private String choiceData;
+	private int choicePID;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_personal_info_edit);
+		baseDataInfos = JoocolaApplication.getInstance().getBaseInfo();
 		ViewUtils.inject(this);
 	}
 
@@ -90,6 +115,148 @@ public class PersonalInfoEditActivity extends Activity {
 			R.id.profession, R.id.annualSalary, R.id.height, R.id.emotion,
 			R.id.smoke, R.id.drink })
 	public void onViewClick(View v) {
+		List<BaseDataInfo> resultInfos = new ArrayList<BaseDataInfo>();
+		TextView display = null;
+		String title = "";
+		switch (v.getId()) {
+		case R.id.hobby:
+
+			break;
+		case R.id.signin:
+
+			break;
+		case R.id.phone:
+
+			break;
+		case R.id.location:
+
+			break;
+		case R.id.profession:
+			for (int i = 0; i < baseDataInfos.size(); i++) {
+				if (baseDataInfos.get(i).getTypeName().equals("Profession")) {
+					resultInfos.add(baseDataInfos.get(i));
+				}
+			}
+			display = profession_tv;
+			title = "职业";
+			break;
+		case R.id.annualSalary:
+			for (int i = 0; i < baseDataInfos.size(); i++) {
+				if (baseDataInfos.get(i).getTypeName().equals("Revenue")) {
+					resultInfos.add(baseDataInfos.get(i));
+				}
+			}
+			display = annualSalary_tv;
+			title = "年收入";
+			break;
+		case R.id.height:
+			for (int i = 0; i < baseDataInfos.size(); i++) {
+				if (baseDataInfos.get(i).getTypeName().equals("Height")) {
+					resultInfos.add(baseDataInfos.get(i));
+				}
+			}
+			display = height_tv;
+			title = "身高";
+			break;
+		case R.id.emotion:
+			for (int i = 0; i < baseDataInfos.size(); i++) {
+				if (baseDataInfos.get(i).getTypeName().equals("Marry")) {
+					resultInfos.add(baseDataInfos.get(i));
+				}
+			}
+			display = emotion_tv;
+			title = "情感状态";
+			break;
+		case R.id.smoke:
+			for (int i = 0; i < baseDataInfos.size(); i++) {
+				if (baseDataInfos.get(i).getTypeName().equals("Smoke")) {
+					resultInfos.add(baseDataInfos.get(i));
+				}
+			}
+			display = smoke_tv;
+			title = "抽烟";
+			break;
+		case R.id.drink:
+			for (int i = 0; i < baseDataInfos.size(); i++) {
+				if (baseDataInfos.get(i).getTypeName().equals("Drink")) {
+					resultInfos.add(baseDataInfos.get(i));
+				}
+			}
+			display = drink_tv;
+			title = "喝酒";
+			break;
+		default:
+			break;
+		}
+		showSingleChoiceDialog(resultInfos, title, display);
+	}
+
+	private void showSingleChoiceDialog(final List<BaseDataInfo> info,
+			final String title, final TextView tv) {
+		final CustomerDialog cdlg = new CustomerDialog(this,
+				R.layout.dlg_singlechoice);
+		choiceData = "";
+		cdlg.setOnCustomerViewCreated(new CustomerViewInterface() {
+
+			@Override
+			public void getCustomerView(Window window, AlertDialog dlg) {
+				TextView tv_title = (TextView) window
+						.findViewById(R.id.dlg_pe_title);
+				ListView lv_items = (ListView) window
+						.findViewById(R.id.dlg_pe_listview);
+				TextView btn_ok = (TextView) window
+						.findViewById(R.id.dlg_pe_ok);
+				TextView btn_cacel = (TextView) window
+						.findViewById(R.id.dlg_pe_cancel);
+				lv_items.setAdapter(new Dlg_ListView_Adapter(info,
+						PersonalInfoEditActivity.this));
+				choiceData = ((BaseDataInfo) lv_items.getAdapter().getItem(0))
+						.getItemName();
+				choicePID = ((BaseDataInfo) lv_items.getAdapter().getItem(0))
+						.getPID();
+				lv_items.setOnItemClickListener(new OnItemClickListener() {
+
+					@Override
+					public void onItemClick(AdapterView<?> arg0, View arg1,
+							int arg2, long arg3) {
+						choiceData = ((BaseDataInfo) arg0.getAdapter().getItem(
+								arg2)).getItemName();
+						choicePID = ((BaseDataInfo) arg0.getAdapter().getItem(
+								arg2)).getPID();
+						((Dlg_ListView_Adapter) arg0.getAdapter()).setPos(arg2);
+					}
+				});
+				OnClickListener listener = new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						switch (v.getId()) {
+						case R.id.dlg_pe_ok:
+							/**
+							 * 这里要执行网络请求
+							 */
+							tv.setText(choiceData);
+							cdlg.dismissDlg();
+							break;
+						case R.id.dlg_pe_cancel:
+							cdlg.dismissDlg();
+							break;
+						default:
+							break;
+						}
+
+					}
+				};
+				btn_ok.setOnClickListener(listener);
+				btn_cacel.setOnClickListener(listener);
+
+			}
+		});
+		cdlg.showDlg();
+	}
+
+	private void showMultiChoiceDialog(List<BaseDataInfo> info, String title,
+			TextView tv) {
 
 	}
 }
