380da1b217029051db3011d6380a68ec63d92f15
tcmaster <18600416253@163.com>
2014-07-18 23:26:19 +0800

    增加了N个地址

--- Joocola/src/com/android/joocola/utils/Constans.java
+++ Joocola/src/com/android/joocola/utils/Constans.java
@@ -45,4 +45,86 @@ public class Constans {
 	public static final String ISSUE_LOCATIONY = "LocationY";
 	public static final String ISSUE_DESCRIPTION = "Description";
 	public static final String ISSUE_PUBLISHERID = "PublisherID";
+
+	/**
+	 * 修改用户信息接口URL
+	 */
+	/**
+	 * 用户爱好
+	 */
+	public static final String HOBBYURL = "Sys.UserController.SetUserHobbies.ashx";
+	/**
+	 * 用户相册地址
+	 */
+	public static final String ALBUMURL = "Sys.UserController.SetUserAlbumPhotoUrls.ashx";
+	/**
+	 * 用户相册地址
+	 */
+	public static final String PHOTOURL = "Sys.UserController.SetUserPhoto.ashx";
+	/**
+	 * 用户昵称
+	 */
+	public static final String NICKNAMEURL = "Sys.UserController.SetUserNickName.ashx";
+	/**
+	 * 用户性别
+	 */
+	public static final String SEXURL = "Sys.UserController.SetUserSex.ashx";
+	/**
+	 * 用户生日
+	 */
+	public static final String BIRTHDAYURL = "Sys.UserController.SetUserBirthday.ashx";
+	/**
+	 * 用户签名
+	 */
+	public static final String SIGNINURL = "Sys.UserController.SetUserSign.ashx";
+	/**
+	 * 用户手机
+	 */
+	public static final String PHONEURL = "Sys.UserController.SetUserPhone.ashx";
+	/**
+	 * 用户原籍
+	 */
+	public static final String CITYURL = "Sys.UserController.SetUserOldCity.ashx";
+	/**
+	 * 用户现居地
+	 */
+	public static final String NEWCITYURL = "Sys.UserController.SetUserNewCity.ashx";
+	/**
+	 * 设置用户职业
+	 */
+	public static final String PROFESSIONURL = "Sys.UserController.SetUserProfession.ashx";
+	/**
+	 * 设置用户婚姻状况
+	 */
+	public static final String MARRYURL = "Sys.UserController.SetUserMarry.ashx";
+	/**
+	 * 设置用户抽烟状况
+	 */
+	public static final String SMOKEURL = "Sys.UserController.SetUserSmoke.ashxx";
+	/**
+	 * 用户饮酒状况
+	 */
+	public static final String DRINKURL = "Sys.UserController.SetUserDrink.ashx";
+	/**
+	 * 用户年收入状况
+	 */
+	public static final String USERREVENUEURL = "Sys.UserController.SetUserRevenue.ashx";
+	/**
+	 * 用户身高状况
+	 */
+	public static final String HEIGHTURL = "Sys.UserController.SetUserHeight.ashx";
+	/**
+	 * 用户QQ
+	 */
+	public static final String QQURL = "Sys.UserController.SetUserQQ.ashx";
+	/**
+	 * 用户微信
+	 */
+	public static final String MICROQQURL = "Sys.UserController.SetUserMicroQQ.ashx";
+	// /**
+	// * 用户微信
+	// */
+	// public static final String MICROQQURL =
+	// "Sys.UserController.SetUserMicroQQ.ashx";
+
 }
