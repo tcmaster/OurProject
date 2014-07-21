fe012e3fb80e50272e0032939f8fa4bc40c7d884
bb <bb@bb-PC>
2014-07-21 21:12:47 +0800

    大概写了下adpater。。明天在写 好累 下班回家。。

---
+++ Joocola/src/com/android/joocola/adapter/GetIssueItemAdapter.java
@@ -0,0 +1,60 @@
+package com.android.joocola.adapter;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.android.joocola.R;
+import com.android.joocola.entity.GetIssueInfoEntity;
+
+
+public class GetIssueItemAdapter extends BaseAdapter {
+	private List<GetIssueInfoEntity> infos;
+	private Context mContext;
+	private LayoutInflater inflater;
+	private ViewHolder holder;
+	public GetIssueItemAdapter(List<GetIssueInfoEntity> info, Context context) {
+		this.infos = info;
+		this.mContext = context;
+		inflater = LayoutInflater.from(context);
+	}
+	@Override
+	public int getCount() {
+		return infos.size();
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		return infos.get(arg0);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if (convertView == null) {
+			holder = new ViewHolder();
+			convertView =inflater.inflate(
+					R.layout.getissueitem, null);
+			holder.title = (TextView) convertView
+					.findViewById(R.id.getissum_title);
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+		holder.title.setText(infos.get(position).getTitle());
+		return convertView;
+	}
+
+	class ViewHolder {
+		TextView title;
+	}
+}
--- Joocola/src/com/android/joocola/fragment/Releasefragment.java
+++ Joocola/src/com/android/joocola/fragment/Releasefragment.java
@@ -1,12 +1,26 @@
 package com.android.joocola.fragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.android.joocola.R;
+import com.android.joocola.adapter.GetIssueItemAdapter;
+import com.android.joocola.entity.GetIssueInfoEntity;
+import com.android.joocola.view.AutoListView;
+import com.android.joocola.view.AutoListView.OnLoadListener;
+import com.android.joocola.view.AutoListView.OnRefreshListener;
 
 /**
  * 发布预约界面
@@ -14,7 +28,48 @@ import com.android.joocola.R;
  * @author bb
  * 
  */
-public class Releasefragment extends Fragment {
+public class Releasefragment extends Fragment implements OnRefreshListener,
+		OnLoadListener {
+	private String issue_url = "Bus.AppointController.QueryAppoint.ashx";
+	private AutoListView mAutoListView;
+	private List<GetIssueInfoEntity> mEntities = new ArrayList<GetIssueInfoEntity>();
+	private GetIssueItemAdapter getIssueItemAdapter;
+	@SuppressLint("HandlerLeak")
+	private Handler releaseHandler  = new Handler(){
+		public void handleMessage(android.os.Message msg) {
+			switch (msg.what) {
+			case 0:
+				String json = (String) msg.obj;
+				try {
+					JSONObject jsonObject = new JSONObject(json);
+					JSONArray jsonArray = jsonObject.getJSONArray("Entities");
+					for (int i = 0; i < jsonArray.length(); i++) {
+						JSONObject object = jsonArray.getJSONObject(i);
+						GetIssueInfoEntity getIssueInfoEntity = new GetIssueInfoEntity();
+						getIssueInfoEntity.setTitle(object.getString("Title"));
+					}
+					getIssueItemAdapter = new GetIssueItemAdapter(mEntities,
+							getActivity());
+					mAutoListView.setAdapter(getIssueItemAdapter);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				break;
+			case 1:
+				for (int i = 0; i < 10; i++) {
+					GetIssueInfoEntity getIssueInfoEntity = new GetIssueInfoEntity();
+					getIssueInfoEntity.setTitle("hehe");
+					mEntities.add(getIssueInfoEntity);
+				}
+				getIssueItemAdapter = new GetIssueItemAdapter(mEntities,
+						getActivity());
+				mAutoListView.setAdapter(getIssueItemAdapter);
+				break;
+			default:
+				break;
+			}
+		};
+	};
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,6 +80,31 @@ public class Releasefragment extends Fragment {
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_release, container,
 				false);
+		mAutoListView = (AutoListView) view.findViewById(R.id.issue_listview);
+		// HttpPostInterface httpPostInterface = new HttpPostInterface();
+		// httpPostInterface.addParams("ItemsPerPage", 10 + "");
+		// httpPostInterface.addParams("CurrentPage", 1 + "");
+		// httpPostInterface.getData(issue_url, new HttpPostCallBack() {
+		//
+		// @Override
+		// public void httpPostResolveData(String result) {
+		// Message message = Message.obtain();
+		// message.what = 0;
+		// message.obj = result;
+		// releaseHandler.sendMessage(message);
+		// }
+		// });
+		releaseHandler.sendEmptyMessage(1);
 		return view;
+	}
+
+	@Override
+	public void onLoad() {
+
+	}
+
+	@Override
+	public void onRefresh() {
+
 	}
 }
