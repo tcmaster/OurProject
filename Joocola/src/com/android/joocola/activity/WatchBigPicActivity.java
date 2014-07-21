de049db826baa9e4ab984d8e78224f652d8b325e
tcmaster <18600416253@163.com>
2014-07-21 22:47:53 +0800

    今天就写到这吧

--- Joocola/src/com/android/joocola/activity/WatchBigPicActivity.java
+++ Joocola/src/com/android/joocola/activity/WatchBigPicActivity.java
@@ -1,5 +1,7 @@
 package com.android.joocola.activity;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
@@ -8,6 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.android.joocola.R;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
 
 /**
  * 点击图片看大图的界面，这个界面可以将当前的图片保存到本地相册，以及滑动浏览所有图片 这个界面用到了XUtils
@@ -19,24 +23,45 @@ public class WatchBigPicActivity extends Activity {
 	/**
 	 * 展示图片的ViewPager
 	 */
+	@ViewInject(R.id.bigPic_vp)
 	private ViewPager photo_vp;
+	/**
+	 * 图片地址
+	 */
+	private List<String> imgUrls;
+	/**
+	 * 当前点击位置
+	 */
+	private int currentItem;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_watch_big_pic);
+		ViewUtils.inject(this);
+		imgUrls = getIntent().getStringArrayListExtra("imgUrls");
+		currentItem = getIntent().getIntExtra("position", 0);
+	}
+
+	private void initActionBar() {
+
+	}
+
+	private void initViewPager() {
+		photo_vp.setAdapter(new Photo_Vp_Adapter());
+		photo_vp.setCurrentItem(currentItem);
 	}
 
 	private class Photo_Vp_Adapter extends PagerAdapter {
 
 		@Override
 		public int getCount() {
-			return 0;
+			return imgUrls.size();
 		}
 
 		@Override
 		public boolean isViewFromObject(View arg0, Object arg1) {
-			return false;
+			return arg0 == arg1;
 		}
 
 		@Override
