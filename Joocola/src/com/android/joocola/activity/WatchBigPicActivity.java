0ee97c1fbd172075ce5e11c45425edb81c65633b
tcmaster <18600416253@163.com>
2014-07-21 18:08:40 +0800

    提交提交

--- Joocola/src/com/android/joocola/activity/WatchBigPicActivity.java
+++ Joocola/src/com/android/joocola/activity/WatchBigPicActivity.java
@@ -2,6 +2,10 @@ package com.android.joocola.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.android.joocola.R;
 
@@ -12,11 +16,39 @@ import com.android.joocola.R;
  * 
  */
 public class WatchBigPicActivity extends Activity {
+	/**
+	 * 展示图片的ViewPager
+	 */
+	private ViewPager photo_vp;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_watch_big_pic);
+	}
+
+	private class Photo_Vp_Adapter extends PagerAdapter {
+
+		@Override
+		public int getCount() {
+			return 0;
+		}
+
+		@Override
+		public boolean isViewFromObject(View arg0, Object arg1) {
+			return false;
+		}
+
+		@Override
+		public Object instantiateItem(ViewGroup container, int position) {
+			return super.instantiateItem(container, position);
+		}
+
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object object) {
+			super.destroyItem(container, position, object);
+		}
+
 	}
 
 }
