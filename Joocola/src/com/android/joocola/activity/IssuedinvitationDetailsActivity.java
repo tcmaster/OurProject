02c3f608a32a16c5f11bbfc27a644a7d8eeca261
bb <bb@172.17.1.143>
2014-07-23 17:35:58 +0800

    已经可以正确传值了

--- Joocola/src/com/android/joocola/activity/IssuedinvitationDetailsActivity.java
+++ Joocola/src/com/android/joocola/activity/IssuedinvitationDetailsActivity.java
@@ -2,6 +2,7 @@ package com.android.joocola.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
@@ -108,7 +109,8 @@ public class IssuedinvitationDetailsActivity extends BaseActivity {
 				Intent intent = new Intent(
 						IssuedinvitationDetailsActivity.this,
 						PersonalDetailActivity.class);
-				intent.putExtra("pid", publishID);
+				intent.putExtra("userId", publishID + "");
+				Log.e("跳转的pid", publishID + "");
 				startActivity(intent);
 				
 			}
